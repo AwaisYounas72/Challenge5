9eb30637816fa3456fd57fff84b6a614c7192339
AwaisYounas72 <151017136+AwaisYounas72@users.noreply.github.com>
2023-12-20 14:25:46 +0500

    Update MenuCard.jsx

--- ecommerce-app/src/Components/MenuCard.jsx
+++ ecommerce-app/src/Components/MenuCard.jsx
@@ -4,15 +4,15 @@ const MenuCard = () => {
     return (
         <div className="main bg-black py-5 rounded-md mt-14 ">
             <div className=" py-5 flex flex-col gap-8 container mx-auto p-3">
-                 <div className="flex flex-row  gap-3 justify-start items-center  ">
+                 <div className="flex flex-row  gap-3 justify-center items-center  ">
                  <div className="text-white text-lg ">
                     We’ve worked with hundreds of amazing people 
                 </div>
-               <hr className="text-[#979797] w-[60%] hidden md:block" />
+               <hr className="text-[#979797] w-[54%] hidden md:block" />
                  </div>
 
                 <div className="flex md:flex-col gap-5  ">
-                    <div className="flex md:flex-row gap-7 flex-wrap">
+                    <div className="flex md:flex-row gap-7 md:gap-28 flex-wrap">
                         <div>
                             <img src="/products/cards/imageU1.png" alt="" />
                         </div>
@@ -26,7 +26,7 @@ const MenuCard = () => {
                             <img src="/products/cards/imageU4.png" alt="" />
                         </div>
                     </div>
-                    <div className="flex md:flex-row gap-7 flex-wrap">
+                    <div className="flex md:flex-row gap-7 md:gap-28 flex-wrap">
                         <div>
                             <img src="/products/cards/imageD1.png" alt="" />
                         </div>
